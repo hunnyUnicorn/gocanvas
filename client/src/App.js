b6ce6f3fd0f36fcf8ae8902f7d77ac31df3a5b51
hunnyUnicorn <hunnyUnicorn1@gmail.com>
2024-12-05 09:36:04 +0100

    create gallery with dummy image

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const cats = [
+    "https://cdn2.thecatapi.com/images/8pl.jpg",
+    "https://cdn2.thecatapi.com/images/8pl.jpg",
+    "https://cdn2.thecatapi.com/images/8pl.jpg",
+    "https://cdn2.thecatapi.com/images/8pl.jpg",
+    "https://cdn2.thecatapi.com/images/8pl.jpg",
+    "https://cdn2.thecatapi.com/images/8pl.jpg",
+    "https://cdn2.thecatapi.com/images/8pl.jpg",
+    "https://cdn2.thecatapi.com/images/8pl.jpg",
+    ];
+    const [loading, setLoading] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Cat Gallery</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="cat-gallery">
+          {cats.map((cat, index) => (
+            <div key={index} className="cat-item">
+              <img src={cat} alt="Cat"/>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
